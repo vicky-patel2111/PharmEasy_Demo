4232cccd199825ed05211f75a438a8e4885d7245
Vihang Patel <vihangpatel22@gmail.com>
2015-12-29 12:21:54 +0530

    Update SlideShowView.java

--- app/src/main/java/in/pharmeasy/demo/slideshow/SlideShowView.java
+++ app/src/main/java/in/pharmeasy/demo/slideshow/SlideShowView.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import in.pharmeasy.demo.data.model.Medicine;
 
-/**
- * Created by Jignesh on 26/12/15.
- */
+
 public interface SlideShowView {
     void slideShow(List<Medicine> medicines);
 }
